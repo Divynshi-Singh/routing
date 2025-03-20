eb9f618375d449e23eb70a0b0ff2489f2898bf64
Divynshi-Singh <divya.singh@innotechteam.in>
2025-03-20 13:35:53 +0530

    created a btn for no-msg

--- src/Pages/MessageDetails.jsx
+++ src/Pages/MessageDetails.jsx
@@ -8,7 +8,17 @@ const MessageDetails = () => {
   const section = messageData.find((item) => item.id === sectionId);
 
   if (!section) {
-    return <div>Section not found</div>;
+    // return  <div>Section not found</div>;
+    return (
+      <div className="modal">
+        <div className="modal-content">
+          <h3>Section Not Found</h3>
+          <p>The section you are looking for does not exist.</p>
+          <button onClick={() => navigate("/message")}>Go Back</button>
+        </div>
+      </div>
+    );
+
   }
 
   return (
