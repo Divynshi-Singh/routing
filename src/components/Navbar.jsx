d2904b588c23d4c50a57d13d08d93528f8eff8e9
Divynshi-Singh <divya.singh@innotechteam.in>
2025-03-20 10:49:12 +0530

    fixed activated-link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-link">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? 'navbar-link active-link' : 'navbar-link')}
+        end
+      >
         About Us
-      </Link>
-      <Link to="/contact" className="navbar-link">
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className={({ isActive }) => (isActive ? 'navbar-link active-link' : 'navbar-link')}
+      >
         Contact Us
-      </Link>
-      <Link to="/message" className="navbar-link">
+      </NavLink>
+      <NavLink
+        to="/message"
+        className={({ isActive }) => (isActive ? 'navbar-link active-link' : 'navbar-link')}
+      >
         Messages
-      </Link>
+      </NavLink>
     </nav>
   );
 };
